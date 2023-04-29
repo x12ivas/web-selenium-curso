01e9d622180b6147f3b65816dc43374125c96440
Windows 10 <Windows 10@DESKTOP-9Q2JR52>
2023-04-29 12:24:27 -0500

    First commit

---
+++ SeleniumCursoRivas/src/test/java/com/google/qa/FirstTest.java
@@ -0,0 +1,24 @@
+package com.google.qa;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstTest {
+  @Test
+  public void f() {
+	  ChromeOptions chromeOpt = new ChromeOptions();
+		WebDriverManager.chromedriver().setup();
+		//chromeOpt.addArguments("user-data-dir=/Users/ricardoavalos/Library/Application Support/Google/Chrome/Default");
+		WebDriver driver = new ChromeDriver(chromeOpt);
+
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+
+		Assert.assertEquals(driver.getCurrentUrl(), "https://www.google.com/");
+  }
+}
